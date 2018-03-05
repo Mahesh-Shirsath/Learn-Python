2100d7d34b0e537b1280262302f6c2458e2303c4
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-06 01:45:28 +0530

    Here we do some for loop related problems

---
+++ for.py
@@ -0,0 +1,17 @@
+# here we are goning to discuss about for loop
+
+# in for loop we use the function name in range
+# it work as range(initial,final,increment or decrement)
+
+for i in range(0,9,1):
+    print "i=",i
+
+# here we doint get output as i=9 because in range function the final index is not taken
+
+for i in range(0,9,2):
+    print "i=",i
+# Here we get all even no as output
+
+for i in range(9,0,-1):
+    print "i=",i
+# here we get revese order as an output
