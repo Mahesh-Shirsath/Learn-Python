bf6000cf7f6c1b94401f87f158ffaee46daeb4fc
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-06 01:36:00 +0530

    This is about if loops

---
+++ ifstatment.py
@@ -0,0 +1,19 @@
+# Here we are going to dicuss about if loops
+
+x=12
+y=13
+if x==y:
+    print "True"
+else:
+    print "False"
+# This is for two conditional statments
+# For more conditins we can us else if condition as elif
+z=13
+if x==y:
+    print "x is equal to y"
+elif y==z:
+    print "z is equal to y"
+elif x==z:
+    print "x is equal to z"
+else:
+    print "Nothing is equal"
