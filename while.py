7e003aaa60515242abb4e7d69270d755c7fa6fef
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-06 02:00:06 +0530

    Here is info about while loop

---
+++ while.py
@@ -0,0 +1,15 @@
+# here we are goning to discuss about while loop
+
+i=0
+while i!=12:
+    print "i=",i
+    i=i+1   # also we can write as i++ or i+=1 all are having same meaning
+
+# Here we work while as same as for loops
+# but in for we can follow loop up infinte time but we can in while loop
+
+while True:
+    i+=1
+    print i
+    if i==12:
+        break
