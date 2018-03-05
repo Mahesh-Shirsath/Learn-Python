b1f33578f771347a7ba45bcd7f419d531476d6a0
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-06 01:35:01 +0530

    This is about variables and lists

---
+++ start.py
@@ -0,0 +1,16 @@
+# In this program we are going to see how to take inputs in python 2.7 and declair variables and lists
+
+var=123      #Here we dirctly declair variables no need of data type
+lists=[12,23,34]    #Here we creat alist of no same for float values and strings
+inp=raw_input("Enter the input : ")
+
+# Addition of variables
+x=13
+y=23
+z=x+y
+print "z="+z # Which is z=36
+
+# Addition of lists
+lis=["mahesh","IT"]
+add=lists+lis
+print add   # Which is [12,23,34,'mahesh','IT']
