113055be2a58856afa2b9afbd0db1459e0464a0e
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-06 11:51:46 +0530

    Here we learn about declaration about functions and call

---
+++ function.py
@@ -0,0 +1,18 @@
+# Here we are going to decleair functions and call it
+
+# we use def keyword for decleair function
+
+def function():
+     print "You are in function"
+
+function()
+
+# we can pass parameter in functions as
+
+def parameter(n,m):
+    for i in range(0,n):
+        for j in range(0,m):
+            print "*",  #In Python 3.x instead of "," we use the function  end=""
+        print ""
+parameter(2,2)
+# Here we from a matrix of 2 by 2
